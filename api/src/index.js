1a39947e55eeee7f33c102177dbd16d9678ae9df
Kenneth Aasan <kenaasa@gmail.com>
2017-05-05 23:42:27 +0200

    Removes api

--- api/src/index.js
+++
@@ -1,8 +0,0 @@
-// Register babel to have ES6 support on the server
-require('babel-core/register');
-
-// fetch polyfill for node
-require('isomorphic-fetch');
-
-// Start the server
-require('./server');
